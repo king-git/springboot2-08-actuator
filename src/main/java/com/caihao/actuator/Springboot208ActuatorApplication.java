ddf10e637a0529848fe3843ab2754b1de17c9dde
caihao <kingch92@163.com>
2019-09-09 22:17:53 +0800

    Initial commit

---
+++ src/main/java/com/caihao/actuator/Springboot208ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.caihao.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot208ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot208ActuatorApplication.class, args);
+	}
+
+}
